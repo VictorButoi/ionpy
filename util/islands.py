9d3fbdd699ba063b433ae6972d2bc9a5c0c0cdda
Victor Ion Butoi <victor.butoi@gmail.com>
2023-09-05 13:59:34 -0400

    changed islands to work with torch instead

--- util/islands.py
+++ util/islands.py
@@ -18,7 +18,7 @@ def dfs(
     : param array: the array that we are searching
     : param row: the current row index we are checking
     : param col: the current col index we are checking
-    : param visited: a numpy array tracking which 
+    : param visited: a torch tensor tracking which 
     : return: A map corresponding to a single connected component.
     """
     num_rows, num_cols = array.shape
@@ -50,8 +50,8 @@ def get_connected_components(
     """
     Returns a list of images with the same shape as label, where each image 
     corresponds to a binary mask for each connected commponent in label.
-    :param label: A binary 2D numpy array.
-    :return: A list of binary 2D numpy arrays, one for each connected component.
+    :param label: A binary 2D torch tensor.
+    :return: A list of binary 2D torch tensors, one for each connected component.
     """
     rows, cols = label.shape 
     visited = torch.zeros(label.shape, dtype=bool)
@@ -63,7 +63,8 @@ def get_connected_components(
                 component_idxs = dfs(i, j, label, visited)
                 comp_image = torch.zeros_like(label)
                 comp_image[component_idxs[:, 0], component_idxs[:, 1]] = 1
-                connected_components.append(comp_image)
+                # Make sure the island is a bool tensor so it can be used for indexing.
+                connected_components.append(comp_image.bool())
     
     return connected_components
 
