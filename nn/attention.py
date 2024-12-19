c3052f3f13dcba93e43b9671b38bf5d735eea520
Victor Ion Butoi <victor.butoi@gmail.com>
2024-12-18 22:31:50 -0500

    finishing up attention td

--- nn/attention.py
+++ nn/attention.py
@@ -11,24 +11,34 @@ from pydantic import validate_arguments
 @dataclass(eq=False, repr=False)
 class MHA(nn.Module):
 
-    model_dim: int
+    d_model: int
     num_heads: int = 1
 
     def __post_init__(self):
         super().__init__()
-        self.W_Q = nn.Linear(self.model_dim, self.model_dim)
-        self.W_K = nn.Linear(self.model_dim, self.model_dim)
-        self.W_V = nn.Linear(self.model_dim, self.model_dim)
+        # Make sure that we can divid the feature dim by the num of heads.
+        assert self.d_model % self.num_heads == 0, "Num heads must cleanly divide the feature dim."
+        
+        self.d_head = self.d_model // self.num_heads
+        self.W_q = nn.Linear(self.d_model, self.d_model)
+        self.W_k = nn.Linear(self.d_model, self.d_model)
+        self.W_v = nn.Linear(self.d_model, self.d_model)
         # Output projection layer
-        self.out = nn.Linear(self.model_dim, self.model_dim)
+        self.out = nn.Linear(self.d_model, self.d_model)
     
+    def split_heads(self,):
+    
+
+    def combine_heads(self,):
+
+
     def scaled_dot_product(self, Q, K, V):
         # Q: B, L, D
         # K: B, L, D
         # V: B, L, D
 
         # First we get an product of the matrices, resulting in B, L, L
-        return torch.softmax((Q @ K.transpose(1, 2)) / math.sqrt(self.model_dim), axis=-1) @ V
+        return torch.softmax((Q @ K.transpose(1, 2)) / math.sqrt(self.d_model), axis=-1) @ V
     
     def forward(self, x):
         # Input x looks like B, L, D
