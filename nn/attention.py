4d7471f4873a7eaea135ab93281754165845a56d
Victor Ion Butoi <victor.butoi@gmail.com>
2024-12-19 23:17:44 -0500

    we are getting this attention working

--- nn/attention.py
+++ nn/attention.py
@@ -23,7 +23,7 @@ class MHA(nn.Module):
         self.W_q = nn.Linear(self.d_model, self.d_model)
         self.W_k = nn.Linear(self.d_model, self.d_model)
         self.W_v = nn.Linear(self.d_model, self.d_model)
-        
+
         # Output projection layer
         self.out = nn.Linear(self.d_model, self.d_model)
     
@@ -33,6 +33,7 @@ class MHA(nn.Module):
         B, L, _ = x.size()
         # First split the last dimension into num_heads many d_head feature vecs.
         x = x.view(B, L, self.num_heads, self.d_head)
+        print("Split head:", x.shape)
         # Then we transpose the middle two dimensions so that we can do L x L
         x = x.transpose(1, 2)
         return x
@@ -42,6 +43,7 @@ class MHA(nn.Module):
         # of shape B, L, H, D_h and turn it into B, L, D.
         B, L, _, _ = x.size()
         x = x.transpose(1, 2).contiguous() # Always do a contiguous before view.
+        print(x.shape)
         x = x.view(B, L, self.d_model)
         return x
 
@@ -49,13 +51,16 @@ class MHA(nn.Module):
         # Q: B, H, L, D_h
         # K: B, H, L, D_h
         # V: B, H, L, D_h
+        print("qkv:",Q.shape, K.shape, V.shape)
         prod = Q @ K.transpose(-2, -1) # B, H, L, L
+        print("prod:",prod.shape)
         # Then we need to renormalize by the root of D_h because each row the dot scales by D_h.
         scaled_prod = prod / math.sqrt(self.d_head)
         # Then we apply the softmax over the keys.
         weights = torch.softmax(scaled_prod, dim=-1) # B, H, L, L
         # Finally we multiply the weights by V to get the output.
         z = weights @ V
+        print("z:",z.shape)
         return z
     
     def forward(self, x):
@@ -63,10 +68,13 @@ class MHA(nn.Module):
         Q = self.split_heads(self.W_q(x))
         K = self.split_heads(self.W_k(x))
         V = self.split_heads(self.W_v(x))
+
         # Then we do a scaled dot-product
         att = self.scaled_dot_product(Q, K, V)
+
         # Finally we combine the heads
         z = self.combine_heads(att)
+
         # Do the final projection
         z_out = self.out(z)
 
