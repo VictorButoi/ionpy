9166541c91bcec887f8f98f9b63d9e9190fe93e7
Victor Ion Butoi <victor.butoi@gmail.com>
2025-01-13 11:30:15 -0500

    fixed runner funcs to seperate job and exp

--- slite/runner.py
+++ slite/runner.py
@@ -10,16 +10,14 @@ from .utils import log_exp_config_objs, submit_input_check, pop_wandb_callback
 
 
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
-def run_exp(
+def run_job(
+    gpu: str,
     config: Config,
-    gpu: str = "0",
+    job_func: Callable,
     track_wandb: bool = False,
     show_examples: bool = False,
     run_name: Optional[str] = None,
-    experiment_class: Optional[Any] = None,
-    job_func: Optional[Callable] = None
 ):
-    submit_input_check(experiment_class, job_func)
     # Get the config as a dictionary.
     cfg = config.to_dict()
     cfg["log"]["show_examples"] = show_examples
@@ -40,16 +38,45 @@ def run_exp(
         "config": cfg,
         "available_gpus": gpu,
     }
-    if experiment_class is not None:
-        slunner.run_exp(
-            exp_class=experiment_class,
-            **run_args
-        )
-    else:
-        slunner.run_job(
-            job_func=job_func,
-            **run_args
-        )
+    slunner.run_job(
+        job_func=job_func,
+        **run_args
+    )
+
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def run_exp(
+    gpu: str,
+    config: Config,
+    experiment_class: Any,
+    track_wandb: bool = False,
+    show_examples: bool = False,
+    run_name: Optional[str] = None,
+):
+    # Get the config as a dictionary.
+    cfg = config.to_dict()
+    cfg["log"]["show_examples"] = show_examples
+    # If run num undefined, make a substitute.
+    if run_name is not None:
+        log_dir_root = "/".join(cfg["log"]["root"].split("/")[:-1])
+        cfg["log"]["root"] = "/".join([log_dir_root, run_name])
+    # Modify a few things relating to callbacks.
+    if "callbacks" in cfg:
+        # If you don't want to show examples, then remove the step callback.
+        if not show_examples and "step" in cfg["callbacks"]:
+            cfg["callbacks"].pop("step")
+        # If not tracking wandb, remove the callback if its in the config.
+        if not track_wandb:
+            pop_wandb_callback(cfg)
+    # Either run the experiment or the job function.
+    run_args = {
+        "config": cfg,
+        "available_gpus": gpu,
+    }
+    slunner.run_exp(
+        exp_class=experiment_class,
+        **run_args
+    )
 
 
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
