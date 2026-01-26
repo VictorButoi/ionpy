7a69845cb8c8fc36066a23aa4038bc76558740ea
Victor Ion Butoi <victor.butoi@gmail.com>
2026-01-26 16:37:56 -0500

    updating init files

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,6 +1,6 @@
 from torch.nn.functional import cross_entropy
 
-from .accuracy import accuracy, f1_score, correct, precision, recall
+from .accuracy import accuracy, f1_score, auroc, auprc, correct, precision, recall
 from .entropy import binary_cross_entropy
 from .size import model_size
 
